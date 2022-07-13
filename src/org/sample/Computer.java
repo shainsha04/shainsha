ba7b34edbd5269e657d4a695dff4119659b75f39
shainsha04 <shainsha4896@gmail.com>
2022-07-13 18:22:47 +0530

    New checking

--- src/org/sample/Computer.java
+++ src/org/sample/Computer.java
@@ -1,10 +1,15 @@
 package org.sample;
 
 public class Computer {
-public void computerModel() {
-System.out.println("Computer Model is IC1122");
-}
-private void checking() {
-	System.out.println("check for own");
-}
+	public void computerModel() {
+		System.out.println("Computer Model is IC1122");
+	}
+
+	private void checking() {
+		System.out.println("check for own");
+	}
+
+	private void newpersn() {
+		System.out.println("Newperson update");
+	}
 }
